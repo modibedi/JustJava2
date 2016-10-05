bd67ad78fbeb3183664f4badd77cc0295e5ff20b
Herry <hdmodibedi@gmail.com>
2016-10-05 12:11:13 +0200

    sevcond commit

--- app/src/main/java/com/example/ekasi/justjava2/MainActivity.java
+++ app/src/main/java/com/example/ekasi/justjava2/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.ekasi.justjava2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -23,6 +27,12 @@ public class MainActivity extends ActionBarActivity {
     * This method is called when the plus button is clicked.
             */
     public void increment(View view) {
+        if (quantity == 100){
+            //show an error message as a toast
+            Toast.makeText(this, " you cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity = quantity + 1;
         display(quantity);
     }
@@ -30,6 +40,12 @@ public class MainActivity extends ActionBarActivity {
     /*** This method is called when the minus button is clicked.
             */
     public void decrement(View view) {
+        if (quantity == 1){
+            //show an error message as a toast
+            Toast.makeText(this, " you cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
          quantity = quantity - 1;
         display(quantity);
 
@@ -39,32 +55,81 @@ public class MainActivity extends ActionBarActivity {
      /*** This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-          String priceMessage = createOrderSummary(price);
-        displayMessage(priceMessage);
+        // Find the users name
+        EditText nameField = (EditText)findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+
+
+        //figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox =(CheckBox) findViewById(R. id. whipped_cream_checkbox);
+       boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        //figure out if the user wants chocolate topping
+        CheckBox ChocolateCheckBox =(CheckBox) findViewById(R. id. chocolate_checkbox);
+        boolean hasChocolate = ChocolateCheckBox.isChecked();
+
+        //figure out if the user wants froth topping
+        CheckBox AlmondVanillaCreamCheckBox =(CheckBox) findViewById(R. id. almond_vanilla_cream_checkbox);
+        boolean hasAlmondVanillaCream = AlmondVanillaCreamCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate, hasAlmondVanillaCream);
+          String priceMessage = createOrderSummary(name,price, hasWhippedCream, hasChocolate, hasAlmondVanillaCream );
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
     }
+
+
+
+
+
 /**
  * Calculates the price of the order
  * @return total price
+ * * @param addWhippedCream is whether or not the user wants whipped cream topping
+ * * @param addAlmondVanillaCream is whether or not the user wants almond vanilla cream topping
+ * * @param addChocolate is whether or not the user wants chocolate cream topping
  */
 
-    private int calculatePrice(){
-        int price = quantity * 5;
-        return price ;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate, boolean addAlmondVanillaCream){
+        int basePrice = 5;
+        //add R2.00 if the user wants Whipped Cream
+        if (addWhippedCream){
+            basePrice = basePrice + 2;
+        }
+        //add R3.00 if the user wants Chocolate
+        if (addChocolate){
+            basePrice = basePrice + 3;
+        }
+        //add R8.00 if the user wants Almond Vanilla Cream
+        if (addAlmondVanillaCream){
+            basePrice = basePrice + 8;
+        }
+
+
+        return quantity * basePrice;
     }
 
     /**
      * create summary of the order
-     *
      * @param price
-     * @return text summary
+     * @param name of customer
+     * @param addWhippedCream is whether or not the user wants whipped cream topping  @return text summary
      */
-
-
-    private String createOrderSummary(int price){
-        String priceMessage = "Name: Herold Modibedi";
-        priceMessage = priceMessage + "Quantity: " + quantity;
-        priceMessage = priceMessage + "Total: R" + price;
+    private String createOrderSummary(String name, int price,boolean addWhippedCream, boolean addChocolate,boolean addAlmondVanillaCream){
+        String priceMessage = "Name: " + name;
+        priceMessage += "\nadd whipped cream?" + addWhippedCream;
+        priceMessage += "\nadd Chocolate?" + addChocolate;
+        priceMessage += "\nadd Almond Vanilla Cream?" + addAlmondVanillaCream;
+        priceMessage = priceMessage + "\nQuantity: " + quantity;
+        priceMessage = priceMessage + "\nTotal: R" + price;
         priceMessage=priceMessage + " \nThank you, please come again!!";
         return priceMessage;
     }
@@ -78,12 +143,7 @@ public class MainActivity extends ActionBarActivity {
         quantityTextView.setText("" + numberOfCoffees);
      }
 
-    /**
-     * this method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+
     }
-}
+
 
